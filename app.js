a2a8a71340282b5036282f4e53c6b4880d358815
Divik Juneja <junejadivik17@gmail.com>
2022-09-10 17:55:55 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 let txtInput = document.querySelector("#input");
 let txtOutput = document.querySelector("#output");
-let btnTranslate = document.querySelector("btn-translate");
+let btnTranslate = document.querySelector("#btn-translate");
 
-let serverURL = "https://api.funtranslations.com/translate/sith.json";
+//let serverURL = "https://api.funtranslations.com/translate/sith.json";
+
+let serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 function constructURL(){
   return serverURL + "?" + "text=" + txtInput.value;
 }
 
-fetch(constructURL())
-.then(response => response.json())
-.then(json => console.log(json))
+function errorHandler(error) {
+    alert("Error has occured. Try again after some time!")
+    console.log(error);
+}
+function clickHandler(){
+    fetch(constructURL())
+    .then(response => response.json())
+    .then(json => txtOutput.innerText = json.contents.translated)
+    .catch(errorHandler);
+}
+
+
+btnTranslate.addEventListener("click", clickHandler);
+// txtOutput.innerText = json.contents.translated
